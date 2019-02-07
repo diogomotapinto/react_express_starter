33e0fc7ef2d80856945abf68c357b08ca4754756
Diogo Mota Pinto <diogomotapinto@gmail.com>
2019-02-07 20:18:31 +0000

    mongodb implemented

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const app = express();
 const jsonParser = bodyParser.json();
+
+const URL = 'mongodb://localhost:27017/customers';
 
 let customers = [
   { id: 1, firstName: 'John', lastName: 'Doe' },
@@ -10,6 +12,37 @@ let customers = [
   { id: 3, firstName: 'Mary', lastName: 'Swanson' },
 ];
 
+/// mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect(URL, { useNewUrlParser: true });
+
+const customersSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+});
+
+const Customer = mongoose.model('Customer', customersSchema);
+
+const addCustomer = new Customer({
+  firstName: 'Elon',
+  lastName: 'Musk'
+})
+
+/*
+addCustomer.save((err, doc) => {
+  if (err) return console.log(err)
+  console.log(doc);
+})
+*/
+
+
+
+Customer.find({ firstName: /^Elon/ }, (err, data) => {
+  if (err) return console.log(err);
+  customers = data
+});
+
+//////////////
 app.get('/api/customers', (req, res) => {
   res.json(customers);
 });
@@ -17,7 +50,7 @@ app.get('/api/customers', (req, res) => {
 app.post('/api/enteruser', jsonParser, (req, res) => {
   customers = [...customers, req.body];
   res.sendStatus(200);
-  console.log(customers);
+
 });
 
 const port = 3001;
